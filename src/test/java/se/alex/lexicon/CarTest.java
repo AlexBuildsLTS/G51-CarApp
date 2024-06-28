fadd7a4307e268fe4521050704cba48f031a9da5
FullStackOS <alex.youssef@live.com>
2024-06-28 08:46:31 +0200

    CarApp

---
+++ src/test/java/se/alex/lexicon/CarTest.java
@@ -0,0 +1,43 @@
+package se.alex.lexicon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void testCarCreation() {
+        Car car = new Car("123ABC", "Volvo", "XC90", 2021);
+        assertEquals("123ABC", car.getRegistrationNumber());
+        assertEquals("Volvo", car.getBrand());
+        assertEquals("XC90", car.getModel());
+        assertEquals(2021, car.getYear());
+        assertNull(car.getOwner());
+    }
+
+    @Test
+    void testSetOwner() {
+        Car car = new Car("123ABC", "Volvo", "XC90", 2021);
+        Person person = new Person("12345", "Alex", "alex@lexicon.com", "123-456-7890");
+        car.setOwner(person);
+        assertEquals(person, car.getOwner());
+    }
+
+    @Test
+    void testAddOwner() {
+        Car car = new Car("123ABC", "Volvo", "XC90", 2021);
+        Person person = new Person("12345", "Alex", "alex@lexicon.com", "123-456-7890");
+        car.addOwner(person);
+        assertEquals(person, car.getOwner());
+    }
+
+    @Test
+    void testRemoveOwner() {
+        Car car = new Car("123ABC", "Volvo", "XC90", 2021);
+        Person person = new Person("12345", "Alex", "alex@lexicon.com", "123-456-7890");
+        car.addOwner(person);
+        car.removeOwner();
+        assertNull(car.getOwner());
+    }
+}
